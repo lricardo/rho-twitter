2b945b077342cf3c0783e0ab95c40aaa4f3a63ee
Leandro Ricardo <leandro.j.c.ricardo@gmail.com>
2018-04-10 17:26:43 +0100

    Reorganization for better readability.

--- app/src/main/java/pt/rhosystems/rhotwitter/activities/MainActivity.java
+++ app/src/main/java/pt/rhosystems/rhotwitter/activities/MainActivity.java
@@ -1,7 +1,6 @@
 package pt.rhosystems.rhotwitter.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -27,13 +26,15 @@ import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
-public class MainActivity extends AppCompatActivity implements StatusListener {
+public class MainActivity extends AppCompatActivity implements StatusListener,
+        SearchView.OnQueryTextListener {
 
     private TwitterStreamFactory twitterStreamFactory;
     private StatusAdapter statusAdapter;
     private List<Status> statusList;
     private RecyclerView tweetListRecyclerView;
     private TwitterStream currentStream;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +51,16 @@ public class MainActivity extends AppCompatActivity implements StatusListener {
         tweetListRecyclerView.setAdapter(statusAdapter);
         tweetListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        buildTwitterBaseConfiguration();
+    }
+
+    /* Twitter4j Related Configurations */
+
+    /**
+     * Build Twiiter4j base configuration and initializes the activity TwitterStreamFactory
+     * with the base authentication.
+     */
+    public void buildTwitterBaseConfiguration() {
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
         configurationBuilder
                 .setOAuthConsumerKey(getBaseContext().getString(R.string.consumer_key))
@@ -60,49 +71,41 @@ public class MainActivity extends AppCompatActivity implements StatusListener {
         Configuration configuration = configurationBuilder.build();
 
         twitterStreamFactory = new TwitterStreamFactory(configuration);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 
+    /** OptionsMenu Configuration for the SearchButton **/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu_main, menu);
 
         final MenuItem searchItem = menu.findItem(R.id.action_search);
-        final SearchView searchView = (SearchView) searchItem.getActionView();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                currentStream = twitterStreamFactory.getInstance();
-                currentStream.addListener(MainActivity.this);
-                currentStream.filter(query);
-
-                searchView.clearFocus();
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                if (newText.isEmpty() && currentStream != null) {
-                    currentStream.shutdown();
-                }
-                return false;
-            }
-        });
+        searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(this);
         return super.onCreateOptionsMenu(menu);
     }
 
+    /** SearchView Callbacks for the text searching capabilities **/
+
     @Override
-    public boolean onSearchRequested() {
-        return super.onSearchRequested();
+    public boolean onQueryTextSubmit(String query) {
+        currentStream = twitterStreamFactory.getInstance();
+        currentStream.addListener(MainActivity.this);
+        currentStream.filter(query);
+
+        searchView.clearFocus();
+        return true;
     }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        if (newText.isEmpty() && currentStream != null) {
+            currentStream.shutdown();
+        }
+        return false;
+    }
+
+    /** Twitter4j Callbacks **/
 
     @Override
     public void onStatus(final Status status) {
@@ -141,4 +144,5 @@ public class MainActivity extends AppCompatActivity implements StatusListener {
     public void onException(Exception ex) {
 
     }
+
 }
